3718787fe25d146f2f4814f3307b3f1b98df2599
Jennifer Pajalla Bravo <jhenchem0210@gmail.com>
2023-02-27 02:54:50 +0800

    modified:   CSS/header.css 	modified:   CSS/home.css 	new file:   CSS/mode.css 	modified:   CSS/style.css 	new file:   CSS/styles.css 	deleted:    README.md 	new file:   connection/connection.php 	new file:   connection/controllerUserData.php 	new file:   connection/forgot-password.php 	new file:   connection/login.php 	new file:   connection/logout-user.php 	new file:   connection/new-password.php 	new file:   connection/password-changed.php 	new file:   connection/signup-user.php 	new file:   db/user.sql 	renamed:    index.html -> dummy.html 	modified:   fonts/font-awesome/css/font-awesome.css 	new file:   index.php 	new file:   javascript/mode.js 	modified:   join/profile3.html 	renamed:    login/home.html -> login/ehom.html

---
+++ connection/controllerUserData.php
@@ -0,0 +1,92 @@
+<?php 
+session_start();
+require "connection.php";
+$email = "";
+$name = "";
+$errors = array();
+
+//if user signup button
+if(isset($_POST['signup'])){
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $password = mysqli_real_escape_string($con, $_POST['password']);
+    $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+    if($password !== $cpassword){
+        $errors['password'] = "Confirm password not matched!";
+    }
+    $email_check = "SELECT * FROM usertable WHERE email = '$email'";
+    $res = mysqli_query($con, $email_check);
+    if(mysqli_num_rows($res) > 0){
+        $errors['email'] = "Email that you have entered is already exist!";
+    }
+    if(count($errors) === 0){
+        $encpass = password_hash($password, PASSWORD_BCRYPT);
+       
+        $insert_data = "INSERT INTO usertable (name, email, password)
+                        values('$name', '$email', '$encpass')";
+        $data_check = mysqli_query($con, $insert_data);
+       
+        }
+                      header('location: ../join/profile1.html');
+        }
+    
+
+    //if user click login button
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $check_email = "SELECT * FROM usertable WHERE email = '$email'";
+        $res = mysqli_query($con, $check_email);
+        if(mysqli_num_rows($res) > 0){
+            $fetch = mysqli_fetch_assoc($res);
+            $fetch_pass = $fetch['password'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                
+                if($status == 'verified'){
+                  $_SESSION['email'] = $email;
+                  $_SESSION['password'] = $password;
+                    header('location: ./connection/login.php');
+                }else{
+                   
+                    $_SESSION['info'] = $info;
+                    header('location: ./index.php');
+                }
+            }else{
+                $errors['email'] = "Incorrect email or password!";
+            }
+        }else{
+            $errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
+        }
+    }
+
+   
+
+    //if user click change password button
+    if(isset($_POST['change-password'])){
+        $_SESSION['info'] = "";
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
+        if($password !== $cpassword){
+            $errors['password'] = "Confirm password not matched!";
+        }else{
+            $code = 0;
+            $email = $_SESSION['email']; //getting this email using session
+            $encpass = password_hash($password, PASSWORD_BCRYPT);
+            $update_pass = "UPDATE usertable SET  password = '$encpass' WHERE email = '$email'";
+            $run_query = mysqli_query($con, $update_pass);
+            if($run_query){
+                $info = "Your password changed. Now you can login with your new password.";
+                $_SESSION['info'] = $info;
+                header('Location: connection/password-changed.php');
+            }else{
+                $errors['db-error'] = "Failed to change your password!";
+            }
+        }
+    }
+    
+   //if login now button click
+    if(isset($_POST['login-now'])){
+        header('Location: ./connection/login.php');
+    }
+?>
